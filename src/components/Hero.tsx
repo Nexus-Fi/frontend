62bb8c3fc7c927baeb13f3d23b88e5ac3522472c
0xShikhar <shikhar@iiitmanipur.ac.in>
2024-05-30 03:27:34 +0530

    added components

---
+++ src/components/Hero.tsx
@@ -0,0 +1,16 @@
+"use client";
+
+import React, { useEffect } from 'react';
+import Link from 'next/link';
+import { BsArrowRight } from 'react-icons/bs';
+
+const Hero = () => {
+
+    return (
+        <div className='text-[30px]'>
+            Dashboard
+        </div>
+    );
+};
+
+export default Hero;
