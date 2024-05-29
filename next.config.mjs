75213c807c923907bfa89ad1dd72cc2b051f2084
0xShikhar <shikhar@iiitmanipur.ac.in>
2024-05-30 01:56:16 +0530

    repo config

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    reactStrictMode: true,
+}
+
+module.exports = {
+	env: {
+	},
+};
 
 export default nextConfig;
