72fc238fbf98098011db5f392dd8fecf0bd586e2
Nikhil <ftnikhil.eth@gmail.com>
2024-06-27 00:06:49 +0530

    feat: fetch query hook

--- src/lib/address.ts
+++ src/lib/address.ts
@@ -1,4 +1,4 @@
-export const STAKE_CONTRACT_ADDRESS=""; 
+export const STAKE_CONTRACT_ADDRESS="nibi1r0qdq0emcute0luvg4dxe5qmrvr4tqq4750vfwpjt8jvkj3gqxtq3tp7fq"; 
 export const stNIBITOKEN_CONTRACT_ADDRESS = ""; 
 export const VALIDATOR_REGISTARY_CONTRACT_ADDRESS = ""; 
 export const REWARD_DISPATCHER_CONTRACT_ADDRESS = ""; 
