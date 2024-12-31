5e5826c337d886a0e660b6ce3ba00a6b49f6541e
andropixels <navgire2001@gmail.com>
2024-12-31 16:23:57 +0530

    mainnet address

--- src/lib/address.ts
+++ src/lib/address.ts
@@ -45,8 +45,16 @@ export const rstNIBI_TOKEN_CONTRACT_ADDRESS = "nibi1h3rnkjxargplk88nqg9y0xrwscj5
 
 
 
-export const STAKE_CONTRACT_ADDRESS = "nibi1g3m7u8lee2at58czzpt7rq0arl70atkc4d7nw3csmy5ua3mdaupsdercss";
-export const stNIBITOKEN_CONTRACT_ADDRESS = "nibi1dqaxugcqp23src49vmnnr7t8rjgryuylwml30shcrg5vf9jrjm4s4xs363";
-export const VALIDATOR_REGISTARY_CONTRACT_ADDRESS = "nibi1xh3h6evfmpwchw09jyv9wjm4qnvnfkuw609qakvk27vnsxhauplqtl3c6j";
-export const REWARD_DISPATCHER_CONTRACT_ADDRESS = "nibi1tdg3h3t9hdz8zxmp97jkwadd077th75whmv5ded4t0ww7vdhdqqshuk6yn";
+// export const STAKE_CONTRACT_ADDRESS = "nibi1g3m7u8lee2at58czzpt7rq0arl70atkc4d7nw3csmy5ua3mdaupsdercss";
+// export const stNIBITOKEN_CONTRACT_ADDRESS = "nibi1dqaxugcqp23src49vmnnr7t8rjgryuylwml30shcrg5vf9jrjm4s4xs363";
+// export const VALIDATOR_REGISTARY_CONTRACT_ADDRESS = "nibi1xh3h6evfmpwchw09jyv9wjm4qnvnfkuw609qakvk27vnsxhauplqtl3c6j";
+// export const REWARD_DISPATCHER_CONTRACT_ADDRESS = "nibi1tdg3h3t9hdz8zxmp97jkwadd077th75whmv5ded4t0ww7vdhdqqshuk6yn";
+
+
+// Mainnet address 
+
+export const STAKE_CONTRACT_ADDRESS = "nibi1d7w9kp3ah5p0zpl3q69tfd5zzx8n4g73pd5v5hyeg4zqdptdkvusayjyqq";
+export const stNIBITOKEN_CONTRACT_ADDRESS = "nibi1rgnf7zq9lvkr5u4emfwm54p7tzwvqvuc5la8pp7xusy8z3xp7vaqctrk7j";
+export const VALIDATOR_REGISTARY_CONTRACT_ADDRESS = "nibi1wz4euppv3dvqac69aspd5ttf4x85ynhhmzkrf27sefhv2fyag0kq82sp2u";
+export const REWARD_DISPATCHER_CONTRACT_ADDRESS = "nibi1ptzpn3h0vf6fjpnrg0uj0n66z25zgqflypqk0xf6h92yazy9hcrsztyp8p";
 
